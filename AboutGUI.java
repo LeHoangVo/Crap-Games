8754c7b6a0f11cfe19c613964f381b200a607b04
Le Hoang Vo <120220905+LeHoangVo@users.noreply.github.com>
2024-08-09 11:02:00 -0400

    Update AboutGUI.java

--- AboutGUI.java
+++ AboutGUI.java
@@ -1,19 +1,12 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
-public class AboutGUI extends JFrame{
-	
+public class AboutGUI extends JFrame{	
 	public AboutGUI(){
 		super("About");
 		this.setSize(300, 300);
 		this.setDefaultCloseOperation(HIDE_ON_CLOSE); // exits program when x is clicked
 		this.setLocationRelativeTo(null); // shows up on center of screen
-		this.setLayout(new FlowLayout());
-		
-
-	}
-
-	public static void main(String[] args) {
-
+		this.setLayout(new FlowLayout());		
 	}
 }
